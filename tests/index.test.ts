c2f3eb0bc8993171786996265f2c4249da19d10e
FLzyy <flzyy72@gmail.com>
2023-01-17 12:56:37 +0800

    remove tests

--- tests/index.test.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Testing:
- *
- * This file is what is used to test `index.ts` using the built in node:test module.
- *
- * Delete if requirements are unclear or frequently changes.
- */
-
-import { describe, it } from "node:test";
