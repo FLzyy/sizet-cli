9d68850d9c9fe637886dcb673da66efbb3d9b16a
FLzyy <flzyy72@gmail.com>
2023-01-17 13:18:07 +0800

    basic cli

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-/**
- * Index:
- *
- * Main entrypoint for your package.
- */
+import { Command } from "commander";
+import { remote, local } from "sizet";
+import { createSpinner } from "nanospinner";
+
+const program = new Command();
+
+// Metadata
+
+program
+  .name("sizet-cli")
+  .description("A CLI tool to easily calculate the size of dependencies.")
+  .version("1.0.0", "-v, --version");
+
+// Commands
+
+program
+  .command("remote <name>", "calculate the size of npm package")
+  .command("local <src>", "calculate the size of a folder with package.json");
+
+// Options
+
+program
+  .option("-d, --verbose", "enables verbose logging for npm commands ran")
+  .option("-t, --temp <prefix>", "prefix for the temporary folder created")
+  .option("-o, --output <path>", "path for a file to output json to");
+
+program.parse();
