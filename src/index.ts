fd71dd1b11f4b03d27609ffc053521345be9d3c6
FLzyy <flzyy72@gmail.com>
2023-01-17 15:08:30 +0800

    functionality 🎉

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 #! /usr/bin/env node
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/no-confusing-void-expression */
 
-import { Command, CommanderError } from "commander";
+import { Command, Argument } from "commander";
 import * as actions from "sizet";
-import { createSpinner } from "nanospinner";
+import { writeFileSync } from "fs";
 
 const program = new Command();
-let value = "";
+let command = "";
+let value: string[] = [];
 
 // Metadata
 
@@ -18,8 +21,15 @@ program
 // Arguments
 
 program
-  .argument("<value>", "the folder or package name to be calculated")
-  .action((val) => {
+  .addArgument(
+    new Argument(
+      "<command>",
+      "the command used to calculate a package"
+    ).choices(["remote", "local"])
+  )
+  .argument("<value...>", "the folder or package name to be calculated")
+  .action((cmd, val) => {
+    command = cmd;
     value = val;
   });
 
@@ -27,15 +37,40 @@ program
 
 program
   .option("-d, --verbose", "enables verbose logging for npm commands ran")
-  .option("-t, --temp <prefix>", "prefix for the temporary folder created")
+  .option("-t, --tempDir <prefix>", "prefix for the temporary folder created")
   .option("-o, --output <path>", "path for a file to output json to");
 
 // Error Formatting
 
 program.configureOutput({
-  writeErr: (str) => process.stdout.write(`\x1b[31m✘\x1b[0m ${str}`),
+  writeErr: (str) => process.stderr.write(`\x1b[31m✘\x1b[0m ${str}`),
 });
 
 // Main Code
 
 program.parse();
+
+console.clear();
+try {
+  const final: Record<string, { tarGzipped: number; unpacked: number }> = {};
+  const run = actions[command];
+  const opts = program.opts();
+
+  const length = value.length;
+  for (let i = 0; i < length; i++) {
+    final[value[i]] = run(value[i], {
+      tempDir: opts.tempDir,
+      verbose: opts.verbose,
+    });
+
+    console.log(`\x1b[1m${value[i]}\x1b[0m:
+    ● Tarred and Gzipped: ${final[value[i]].tarGzipped / 1000}kb
+    ● Unpacked: ${final[value[i]].unpacked / 1000}kb`);
+  }
+
+  if (opts.output) {
+    writeFileSync(opts.output, JSON.stringify(final, null, "\t"));
+  }
+} catch (error) {
+  console.error(`\x1b[31m✘\x1b[0m ${error}`);
+}
